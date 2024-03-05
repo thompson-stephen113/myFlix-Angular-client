a16ea8ab2878cfbda1201a4e40c58c2cc6fe2344
Stephen Thompson <thompson.stephen113@gmail.com>
2024-03-05 09:50:59 -0500

    imported various modules
    Imported BrowserModule, HttpClientModule, FormsModule, NgModule, BrowserAnimationsModule, and various Material modules.
    Imported AppComponent, RegistrationComponent, and LoginComponent, and added them to the declarations array.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RegistrationComponent } from "./registration/registration.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        RegistrationComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
-        CommonModule,
         HttpClientModule,
-        AppRoutingModule
+        FormsModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatDialogModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSnackBarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
