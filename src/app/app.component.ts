7e23e6733e918160fe92fb6651b828438c2995cc
Stephen Thompson <thompson.stephen113@gmail.com>
2024-03-05 09:52:18 -0500

    imported RegistrationComponent and LoginComponent
    Added functions to open dialogs for when the Sign Up or Login buttons are clicked on the main page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+
+import { RegistrationComponent } from "./registration/registration.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	// Defines component as app root
+	selector: "app-root",
+
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  title = 'myFlix-Angular-client';
-}
+	title = "myFlix-Angular-client";
+
+	// Passes a dialog as an argument so it can be used in this component
+	constructor(public dialog: MatDialog) { }
+
+	// Opens a dialog when the Sign Up button is clicked
+	openUserRegistrationDialog(): void {
+		this.dialog.open(RegistrationComponent, {
+			width: "280px"
+		});
+	};
+
+	// Opens a dialog when the Login button is clicked
+	openUserLoginDialog(): void {
+		this.dialog.open(LoginComponent, {
+			width: "280px"
+		});
+	};
+};
